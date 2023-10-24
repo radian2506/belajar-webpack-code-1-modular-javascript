244e9199e54cf25f073dc276ff7f22b69cac8bd3
radian2506 <radiansyah.pro@gmail.com>
2023-10-24 20:42:49 +0700

    update fitur index dan folder src

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const run = require('./app/app');
+const AlertService = require('./app/alert.service');
+const CalculatorsService = require('./app/calculator.service');
+const JokesService = require('./app/jokes.service');
+
+const alertService = new AlertService();
+const calculatorService = new CalculatorService();
+const jokesService = new JokesService();
+run(alertService, calculatorService, jokesService);
